5c516ace09f0dd87c7adb948268c30ae385aba71
Nicolas Melo <nicolasmelo12@gmail.com>
2020-06-15 00:42:09 -0300

    small changes to data

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -3,5 +3,5 @@
 pypy3 manage.py collectstatic
 pypy3 manage.py migrate
 pypy3 manage.py loaddata fixtures/required_data.json
-pypy3 manage.py loaddata fixtures/initial_data.json
+#pypy3 manage.py loaddata fixtures/initial_data.json
 daphne -b 0.0.0.0 -p 8000 reflow_crm.asgi:application
