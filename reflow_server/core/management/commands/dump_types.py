ab8dcdb1431ade4d1bb1143b47d40889e89a7bcd
Nicolas Melo <nicolasmelo12@gmail.com>
2020-06-18 04:17:35 -0300

    change pypy to python

--- reflow_server/core/management/commands/dump_types.py
+++ reflow_server/core/management/commands/dump_types.py
@@ -13,6 +13,6 @@ class Command(BaseCommand):
         settings_root = re.sub(r'urls$','', settings.ROOT_URLCONF)
         app_names = [app.replace(settings_root, '') for app in settings.INSTALLED_APPS if settings_root in app] 
         app_models = ['{}.{}'.format(app_label, model._meta.object_name) for app_label in app_names for model in apps.get_app_config(app_label).get_models() if re.search(r'Type$', model._meta.object_name)]
-        command = 'pypy3 manage.py dumpdata {} > fixtures/required_data.json'.format(' '.join(app_models))
+        command = 'python manage.py dumpdata {} > fixtures/required_data.json'.format(' '.join(app_models))
         print(command)
         subprocess.run(command.split())
--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #pypy3 manage.py collectstatic
-pypy3 manage.py migrate
-pypy3 manage.py loaddata fixtures/required_data.json
+python manage.py migrate
+python manage.py loaddata fixtures/required_data.json
 #pypy3 manage.py loaddata fixtures/initial_data.json
 daphne -b 0.0.0.0 -p 8000 reflow_server.asgi:application
