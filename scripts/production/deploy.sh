ab6f6173c935fce7c237dd86f829d73ed0b251f7
Nicolas Melo <nicolasmelo12@gmail.com>
2020-06-24 12:21:41 -0300

    fix bugs

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #pypy3 manage.py collectstatic
-python manage.py migrate fake
+python manage.py migrate
 python manage.py loaddata fixtures/required_data.json
 #pypy3 manage.py loaddata fixtures/initial_data.json
 daphne -b 0.0.0.0 -p 8000 reflow_server.asgi:application
