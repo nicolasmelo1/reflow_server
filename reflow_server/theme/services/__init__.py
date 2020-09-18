0b58d58dcecb2d9e10752119881bb723f0fe68f0
Nicolas Melo <nicolasmelo12@gmail.com>
2020-09-18 17:55:07 -0300

    fix issue on notifications send email

--- reflow_server/theme/services/__init__.py
+++ reflow_server/theme/services/__init__.py
@@ -2,5 +2,5 @@ from .select import ThemeSelectService
 
 class ThemeService:
     @staticmethod
-    def select_theme(theme_id, user_id, company_id):
+    def select_theme(theme_id, company_id, user_id):
         return ThemeSelectService(theme_id, company_id, user_id).select()
