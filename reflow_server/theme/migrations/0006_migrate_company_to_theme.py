1512ab0cc905db20cf19dab231dd20eaff9d7879
Nicolas Leal <nicolasmelo12@gmail.com>
2020-12-12 12:40:13 -0400

    fix small bugs in rich text

--- reflow_server/theme/migrations/0006_migrate_company_to_theme.py
+++ reflow_server/theme/migrations/0006_migrate_company_to_theme.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 def migrate_company_to_theme(apps, schema_editor):
     Theme = apps.get_model('theme', 'Theme')
     for theme in Theme.objects.all():
-        if hasattr(theme,'company'):
+        if hasattr(theme,'company') and theme.user:
             theme.company = theme.user.company
             theme.save()
 
