15bfc8ceecd8125404990af401d1b7a038a2c0bb
Nicolas Leal <nicolasmelo12@gmail.com>
2021-05-11 20:02:12 -0300

    added relation

--- reflow_server/listing/relations.py
+++ reflow_server/listing/relations.py
@@ -9,10 +9,21 @@ from reflow_server.formulary.models import Field, Form
 
 class ListingHeaderFieldsRelation(serializers.ModelSerializer):
     id = serializers.IntegerField()
-    
+    conditional = serializers.SerializerMethodField()
+
+    def get_conditional(self, obj):
+        if obj.form.conditional_on_field and obj.form.conditional_type:
+            return {
+                'conditional_field_label_name': obj.form.conditional_on_field.label_name,
+                'conditional_type': obj.form.conditional_type.type,
+                'conditional_value': obj.form.conditional_value
+            }
+        else:
+            return None
+
     class Meta:
         model = Field
-        fields = ('id', 'label_name', 'name', 'type')
+        fields = ('id', 'label_name', 'name', 'type', 'conditional')
 
 
 class ExtractFormValueListSerializer(serializers.ListSerializer):
