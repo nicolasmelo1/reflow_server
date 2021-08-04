88215a608cdff8ab27ad2e23ea9fbd2c8bd465ae
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-08-04 16:01:31 -0300

    deploy

--- scripts/production/pre_deploy.sh
+++ scripts/production/pre_deploy.sh
@@ -4,3 +4,4 @@ cp docker-compose.yml scripts/development/docker-compose.development.yml
 cp scripts/production/docker-compose.production.yml docker-compose.yml
 git add -A
 git commit -m "deploy"
+cp scripts/development/docker-compose.development.yml docker-compose.yml
