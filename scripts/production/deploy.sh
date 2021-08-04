38c97fa4a59aa27e5fab004c752e569a24d74de2
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-08-04 03:50:47 -0300

    teste

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 python manage.py loaddata fixtures/required_data.json
 python manage.py loaddata fixtures/address_data.json
 #pypy3 manage.py loaddata fixtures/initial_data.json
-daphne -b 0.0.0.0 -p 8000 reflow_server.asgi:application
+daphne -p 8000 reflow_server.asgi:application
