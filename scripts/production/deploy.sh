15f59b201e1e4d358a90fdddc783119736532b83
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-08-04 03:56:08 -0300

    teste

--- scripts/production/deploy.sh
+++ scripts/production/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-mkdir static
 python manage.py collectstatic
 python manage.py migrate
 python manage.py loaddata fixtures/required_data.json
 python manage.py loaddata fixtures/address_data.json
 #pypy3 manage.py loaddata fixtures/initial_data.json
-daphne -p 8000 reflow_server.asgi:application
+daphne -b 0.0.0.0 -p 8000 reflow_server.asgi:application
