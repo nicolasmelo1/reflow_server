68db33726ddcea3abf403418233429579ea0c93e
Nicolas Melo <nicolasmelo@MacBook-Pro-de-Nicolas.local>
2021-08-13 20:45:16 -0300

    remove lexer

--- reflow_server/formula/apps.py
+++ reflow_server/formula/apps.py
@@ -5,7 +5,10 @@ class FormulaConfig(AppConfig):
     name = 'reflow_server.formula'
 
     def ready(self):
-        from reflow_server.formula.services.documentation import DocumentationService
+        try:
+            from reflow_server.formula.services.documentation import DocumentationService
 
-        documentation_service = DocumentationService()
-        documentation_service.update_documentation()
+            documentation_service = DocumentationService()
+            documentation_service.update_documentation()
+        except Exception as e:
+            print(e)
